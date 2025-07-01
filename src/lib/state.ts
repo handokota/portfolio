730c7c2dcfefcd61037900abe9e47938def91875
handokota <handokota@protonmail.com>
2025-07-01 21:12:55 +0700

    feat(settings): set default animations to false

--- src/lib/state.ts
+++ src/lib/state.ts
@@ -9,7 +9,7 @@ import type { Settings } from '~/types';
 
 // Note: `createState` needed as without it it looses reactivity for some reason. Needs to be looked into further
 const DEFAULT_STATE = createState<Settings>({
-	animations: true,
+	animations: false,
 	sound: false,
 });
 
