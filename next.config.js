c8a58a3d79b3d1c8d5447f9b1f70a78e505d7a90
handokota <handokota@protonmail.com>
2025-07-24 23:11:38 +0700

    fix(security): update Content Security Policy and add security headers in configuration

--- next.config.js
+++ next.config.js
@@ -33,29 +33,31 @@ const config = {
 			'images.unsplash.com',
 		],
 	},
-	// Inspired by: https://github.com/leerob/leerob.io/blob/main/next.config.js#L44-L81
 	async headers() {
 		return [
 			{
 				source: '/(.*)',
 				headers: [
-					// https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
 					{
 						key: 'Content-Security-Policy',
 						value: ContentSecurityPolicy.replace(/\n/g, ''),
 					},
-					// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
 					{
 						key: 'Referrer-Policy',
 						value: 'strict-origin-when-cross-origin',
 					},
-					// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+					{
+						key: 'X-Content-Type-Options',
+						value: 'nosniff',
+					},
+					{
+    					key: 'X-Frame-Options',
+    					value: 'SAMEORIGIN'
+},
 					{
 						key: 'Strict-Transport-Security',
 						value: 'max-age=31536000; includeSubDomains; preload',
 					},
-					// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-					// Opt-out of Google FLoC: https://amifloced.org/
 					{
 						key: 'Permissions-Policy',
 						value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
